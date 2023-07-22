1d16265c79c6c44cac20669f3d0598e82837cb1c
Mr.pain <1477871185@qq.com>
2023-07-23 00:38:51 +0800

    学生管理系统

--- src/com/StudentSystem.java
+++ src/com/StudentSystem.java
@@ -12,5 +12,14 @@ public class StudentSystem {
         System.out.println("5:退出");
         System.out.println("请输入您的选择：");
         Scanner sc = new Scanner(System.in);
+        String choose = sc.next();
+        switch (choose) {
+            case "1" -> System.out.println("添加学生");
+            case "2" -> System.out.println("删除学生");
+            case "3" -> System.out.println("修改学生");
+            case "4" -> System.out.println("查询学生");
+            case "5" -> System.out.println("退出");
+            default -> System.out.println("没有这个选项");
+        }
     }
 }
