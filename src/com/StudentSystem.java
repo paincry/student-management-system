d34d8c9d2f02370df3b0a40277e8f46b5f08f4fd
Mr.pain <1477871185@qq.com>
2023-08-26 15:28:21 +0800

    Update StudentSystem.java

--- src/com/StudentSystem.java
+++ src/com/StudentSystem.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class StudentSystem {
 
-    public static void main(String[] args) {
+    public static void startStudentSystem() {
         ArrayList<student> list = new ArrayList<>();
         loop:
         while (true) {
